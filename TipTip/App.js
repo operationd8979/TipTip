9c425e3938ca333f2d427f7626e8c5ab5da02140
operationd8979 <operationddd@gmail.com>
2023-09-12 19:53:09 +0700

    add de asb for dev build

--- TipTip/App.js
+++ TipTip/App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Voice from '@react-native-voice/voice';
 
-export default function App() {
+const App = () => {
+
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [text, setText] = useState('');
+
+  const startRecording = async () => {
+    try {
+      await Voice.start('vi-VN'); // Chọn ngôn ngữ (ví dụ: tiếng Việt)
+      setIsRecording(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const stopRecording = async () => {
+    try {
+      await Voice.stop();
+      setIsRecording(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  Voice.onSpeechResults = (event) => {
+    // Xử lý kết quả khi nhận được âm thanh ghi âm thành công
+    setText(event.value[0]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity onPress={isRecording ? stopRecording : startRecording}>
+        <Text>{isRecording ? 'Dừng ghi âm' : 'Bắt đầu ghi âm'}</Text>
+      </TouchableOpacity>
+      <Text>Đoạn văn: {text}</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
